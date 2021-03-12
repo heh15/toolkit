01a26978a58233836c50669b7bc6d320dd991f95
heh15 <explorationdiscovery@hotmail.com>
2021-03-12 15:44:15 -0500

    Modify the beam_get function so it works for multi-Gaussian fitting

--- casaFunctions.py
+++ casaFunctions.py
@@ -1,32 +1,37 @@
 ############################################################
 # function
 
-def beam_get(imagename, region_init, ratio=2.0):
+def beam_get(imagename, region_init, ratio=2.0, **kwarg):
     ''' 
     parameters
     imagename: The path to the CASA image file
     regions: The path to the CASA region file or the CASA 
     region string. 
     ratio: The ratio to be multiplied by the fitted ellipse to 
-    get the final elliptical aperture shape. 
+    get the final elliptical aperture shape.
+    **kwarg: other parameters that goes into the imfit 
     '''
-    beam=imfit(imagename=imagename,region=region_init)
-    x_value=beam['results']['component0']['shape']\
-             ['direction']['m0']['value']
-    y_value=beam['results']['component0']['shape']\
-             ['direction']['m1']['value']
-    bmaj_value=beam['results']['component0']\
-                ['shape']['majoraxis']['value']
-    bmin_value=beam['results']['component0']['shape']\
-                ['minoraxis']['value']
-    pa_value=beam['results']['component0']['shape']\
-              ['positionangle']['value']
-    x=str(x_value)+'rad'
-    y=str(y_value)+'rad'
-    # Get the semi major and minor axis to write in the region file.
-    bmaj_semi=str(bmaj_value/2.0*ratio)+'arcsec'
-    bmin_semi=str(bmin_value/2.0*ratio)+'arcsec'
-    pa=str(pa_value)+'deg'
-    region='ellipse[['+x+','+y+'],['+bmaj_semi+','+bmin_semi+'],'+pa+']'
+    beam=imfit(imagename=imagename,region=region_init, **kwarg)
+    regions = []
+    for i in range(beam['results']['nelements']):
+        component = 'component' + str(i)
+        x_value=beam['results'][component]['shape']\
+                 ['direction']['m0']['value']
+        y_value=beam['results'][component]['shape']\
+                 ['direction']['m1']['value']
+        bmaj_value=beam['results'][component]\
+                    ['shape']['majoraxis']['value']
+        bmin_value=beam['results'][component]['shape']\
+                    ['minoraxis']['value']
+        pa_value=beam['results'][component]['shape']\
+                  ['positionangle']['value']
+        x=str(x_value)+'rad'
+        y=str(y_value)+'rad'
+        bmaj=str(bmaj_value/2.0*ratio)+'arcsec'
+        bmin=str(bmin_value/2.0*ratio)+'arcsec'
+        pa=str(pa_value)+'deg'
+        region='ellipse[['+x+','+y+'],['+bmaj+','+bmin+'],'+pa+']'
+        regions.append(region)
 
-    return region
+    return regions
+
