915f056df46426cdc7b5d68b964ce4735fa6a19d
heh15 <explorationdiscovery@hotmail.com>
2022-01-19 15:54:00 -0500

    Add the function to draw fast 2D contour

---
+++ plotTool.py
@@ -0,0 +1,117 @@
+
+def density_contour(
+        x, y, weights=None, xlim=None, ylim=None,
+        overscan=(0.1, 0.1), logbin=(0.02, 0.02), smooth_nbin=(3, 3),
+        levels=(0.393, 0.865, 0.989), alphas=(0.75, 0.50, 0.25),
+        color='k', contour_type='contourf', ax=None, **contourkw):
+    """
+    This function comes from 
+    https://github.com/astrojysun/Sun_Plot_Tools/blob/master/sun_plot_tools/ax.py#L101
+    Generate data density contours (in log-log space).
+    Parameters
+    ----------
+    x, y : array_like
+        x & y coordinates of the data points
+    weights : array_like, optional
+        Statistical weight on each data point.
+        If None (default), uniform weight is applied.
+        If not None, this should be an array of weights,
+        with its shape matching `x` and `y`.
+    xlim, ylim : array_like, optional
+        Range to calculate and generate contour.
+        Default is to use a range wider than the data range
+        by a factor of F on both sides, where F is specified by
+        the keyword 'overscan'.
+    overscan : array_like (length=2), optional
+        Factor by which 'xlim' and 'ylim' are wider than
+        the data range on both sides. Default is 0.1 dex wider,
+        meaning that xlim = (Min(x) / 10**0.1, Max(x) * 10**0.1),
+        and the same case for ylim.
+    logbin : array_like (length=2), optional
+        Bin widths (in dex) used for generating the 2D histogram.
+        Usually the default value (0.02 dex) is enough, but it
+        might need to be higher for complex distribution shape.
+    smooth_nbin : array_like (length=2), optional
+        Number of bins to smooth over along x & y direction.
+        To be passed to `~scipy.ndimage.gaussian_filter`
+    levels : array_like, optional
+        Contour levels to be plotted, specified as levels in CDF.
+        By default levels=(0.393, 0.865, 0.989), which corresponds
+        to the integral of a 2D normal distribution within 1-sigma,
+        2-sigma, and 3-sigma range (i.e., Mahalanobis distance).
+        Note that for an N-level contour plot, 'levels' must have
+        length=N+1, and its leading element must be 0.
+    alphas : array_like, optional
+        Transparancy of the contours. Default: (0.75, 0.50, 0.25)
+    color : mpl color, optional
+        Base color of the contours. Default: 'k'
+    contour_type : {'contour', 'contourf'}, optional
+        Contour drawing function to call
+    ax : `~matplotlib.axes.Axes` object, optional
+        The Axes object to plot contours in.
+    **contourkw
+        Keywords to be passed to the contour drawing function
+        (see keyword "contour_type")
+    Returns
+    -------
+    ax : `~matplotlib.axes.Axes` object
+        The Axes object in which contours are plotted.
+    """
+    
+    if xlim is None:
+        xlim = (10**(np.nanmin(np.log10(x))-overscan[0]),
+                10**(np.nanmax(np.log10(x))+overscan[0]))
+    if ylim is None:
+        ylim = (10**(np.nanmin(np.log10(y))-overscan[1]),
+                10**(np.nanmax(np.log10(y))+overscan[1]))
+
+    if ax is None:
+        ax = plt.subplot(111)
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+
+    # force to change to log-log scale
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    # generate 2D histogram
+    lxedges = np.arange(
+        np.log10(xlim)[0], np.log10(xlim)[1]+logbin[0], logbin[0])
+    lyedges = np.arange(
+        np.log10(ylim)[0], np.log10(ylim)[1]+logbin[1], logbin[1])
+    if weights is None:
+        hist, lxedges, lyedges = np.histogram2d(
+            np.log10(x), np.log10(y),
+            bins=[lxedges, lyedges])
+    else:
+        hist, lxedges, lyedges = np.histogram2d(
+            np.log10(x), np.log10(y), weights=weights,
+            bins=[lxedges, lyedges])
+    xmids = 10**(lxedges[:-1] + 0.5*logbin[0])
+    ymids = 10**(lyedges[:-1] + 0.5*logbin[1])
+    
+    # smooth 2D histogram
+    pdf = gaussian_filter(hist, smooth_nbin).T
+    
+    # calculate cumulative density distribution (CDF)
+    cdf = np.zeros_like(pdf).ravel()
+    for i, density in enumerate(pdf.ravel()):
+        cdf[i] = pdf[pdf >= density].sum()
+    cdf = (cdf/cdf.max()).reshape(pdf.shape)
+
+    # plot contourf
+    if contour_type == 'contour':
+        contourfunc = ax.contour
+        contourlevels = levels
+    elif contour_type == 'contourf':
+        contourfunc = ax.contourf
+        contourlevels = np.hstack([[0], levels])
+    else:
+        raise ValueError(
+            "'contour_type' should be either 'contour' or 'contourf'")
+    contourfunc(
+        xmids, ymids, cdf, contourlevels,
+        colors=[mpl.colors.to_rgba(color, a) for a in alphas],
+        **contourkw)
+    
+    return ax
