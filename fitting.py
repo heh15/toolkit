f2579769a0fd264bc1fb28df4c01ce1746346e29
heh15 <he_hao_@outlook.com>
2022-02-05 13:05:04 -0500

    Add the function for proportional fitting

---
+++ fitting.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def fit_proportional(x, y):
+    '''
+    Fit the linear relation forced through 0 point
+    ------
+    Parameters:
+    x: np.1darray
+        x of the data
+    y: np.1darray
+        y of the data
+    ------
+    return:
+    a: float
+        coefficient between x and y
+    '''
+    x = x[:,np.newaxis]
+    a,_,_,_ = np.linalg.lstsq()
+    return a
