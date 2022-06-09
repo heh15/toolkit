e5587290c3df09af8779daa36cb5b931f7cec030
heh15 <explorationdiscovery@hotmail.com>
2022-06-09 05:41:16 -0400

    Add 'aproxCalcPWV()' function to calculate the PWV values

--- casaFunctions.py
+++ casaFunctions.py
@@ -183,3 +183,124 @@ def normalize_Tsys(Tsys_sinspw, isin_phase, isin_sci, isin_bpass, normScans=[0,0
             Tsys_norm[isin] = Tsys_sinspw[isin] / Tsys_sinspw[isin[0][normScans[i]]]
 
     return Tsys_norm
+
+def approxCalcPWV(tsrc0,tsrc1,tsrc2,tsrc3,m_el,Tamb=275,r=[1.193,0.399,0.176,0.115],
+               tau0=[0.027,0.02,0.01,0.01],eta_c=0.97,verb=True):
+    '''
+    Calculate the pwv values from the Twvr from 4 channels
+    ------
+    Parameters
+    tsrc0, tsrc1, tsrc2, tsrc3:
+        WVR temperature for channel 0, 1, 2, 3
+    m_el: float
+        Elevation in degrees
+    Tamb: float
+        Ambient temperature in Kelvin
+    r: list
+        ratio of opacity at each filter compared to PWV
+    tau0: list
+        contribution of the continuum
+    eta_c: float
+        The forward efficiency of the antenna
+    verb: bool
+        If true, print out bunch of information
+    ------
+    Return
+    pwv_z:
+        Zenith pwv values
+    '''
+    m_el=m_el/57.295   # convert to radians
+    T_loss=275.0
+
+    if tsrc0 > T_loss: T_loss = tsrc0
+
+    tsrc0=(tsrc0-(1.0-eta_c)*T_loss)/eta_c
+    tsrc1=(tsrc1-(1.0-eta_c)*T_loss)/eta_c
+    tsrc2=(tsrc2-(1.0-eta_c)*T_loss)/eta_c
+    tsrc3=(tsrc3-(1.0-eta_c)*T_loss)/eta_c
+
+    pw=[0.0,0.0,0.0,0.0]; pw_noc=[0.0,0.0,0.0,0.0]
+    site="AOS"
+
+    if site=="AOS":
+    # approximate physical temp of atmosphere ,based on ambient temperature, Tamb in Kelvin.
+       Tphys=Tamb
+
+    if tsrc0 > Tphys: Tphys = tsrc0
+    if verb: print(site, Tphys)
+
+    tel=[0.0,0.0,0.0,0.0]
+    tz=[0.0,0.0,0.0,0.0]
+    wt=[0.0,0.0,0.0,0.0]
+
+    # calculates transmissions:
+    tel[3]=(1.0-tsrc3/Tphys)
+    tel[2]=(1.0-tsrc2/Tphys)
+    tel[1]=(1.0-tsrc1/Tphys)
+    tel[0]=(1.0-tsrc0/Tphys)
+
+    if verb: print("Ta:",tsrc0,tsrc1,tsrc2,tsrc3)
+    if verb: print("tel", tel)
+    for i in range(4):
+        if tel[i]<0.0001: tel[i]=0.0001
+        wt[i]=1.0-(abs(tel[i]-0.5)/0.5)**0.5  # weights
+    if verb: print('weights ',wt)
+    use=1
+    for i in range(4):
+           pw[i]=-(np.log(tel[i])+tau0[i])/r[i]
+    if verb: print('uncorrected pwv',pw)
+
+# wet components
+    rat31_1=pw[3]/pw[1]
+    pwm=np.mean(pw)
+    if pwm>0.5:   # only look for a wet cloud component if pwv>0.5mm   (bit arbitrary cutoff but probably ok)
+        pwt=np.zeros(4)
+        tauc0_0=-0.02
+        tauc0=0.01
+        i=0
+        std_pwt_old=9999.0
+        iloop=True
+        std_pwt=np.zeros(25);tauc=np.zeros(25)
+        while(iloop):
+            tauc[i]=tauc0_0 + tauc0*1.17**i-tauc0
+            for i1 in range(4):
+                pwt[i1]=-(np.log(tel[i1])+tau0[i1]+tauc[i])/r[i1]
+                if pwt[i1]<0.0: iloop=False
+            mean_pwt,std_pwt[i]=weighted_avg_and_std(pwt, wt)   # get slope of 4 pwv values, using weights (should weight down channels 0,1 a lot)
+
+            if abs(std_pwt[i])>std_pwt_old or (abs(std_pwt[i])/mean_pwt < 0.000001) : iloop=False # stop loop if slope is getting larger, or diff is <0.001%
+            if verb: print('tauc',tauc[i],'std(pwv)',std_pwt[i],'pwv',pwt)
+            std_pwt_old=std_pwt[i]
+            i+=1
+            if i>24: iloop=False
+
+
+        tau_constant=tauc[i-2]
+#   print 'tauc:',tau_constant    # this is the last but one estimate, before it started increasing again
+    else:
+        tau_constant=0.0  # default, for low pwv
+
+#   re-estimates pwv, after removing additional tau_constant component
+    for i in range(4):
+           pw_noc[i]=-(np.log(tel[i])+tau0[i]+tau_constant)/r[i]
+    if verb: print('corrected:',pw_noc)
+    rat31_2=pw[3]/pw[1]
+    ratio31_a=pw[3]/pw[1]
+
+#   estimates weighted mean pwv, with and without cloud component:
+    ws=0.0
+    for i in range(4):
+          ws=ws+pw[i]*wt[i]
+    pwv_los=ws/sum(wt)
+    pwv_z=pwv_los*math.sin(m_el)
+
+#   wet components
+#   now optionally remove wet cloud component
+    ws=0.0
+    for i in range(4):
+          ws=ws+pw_noc[i]*wt[i]
+    pwv_los_noc=ws/sum(wt)
+    pwv_z_noc=pwv_los_noc*math.sin(m_el)
+    tau_constant_z=tau_constant      # *math.sin(m_el)   ##!! assume tau_constant is not planar - just the line of sign value (ie don't use sin(elev) )
+
+    return pwv_z
